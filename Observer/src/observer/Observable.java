140d3f707a2c4651ecb054fd18635a580a1281c2
StefanyStefanova <stefani.d.s@abv.bg>
2020-04-13 17:10:03 +0300

    Add observer pattern

---
+++ Observer/src/observer/Observable.java
@@ -0,0 +1,11 @@
+package observer;
+
+public interface Observable {
+    public void subscribe(Observer observer);
+    public void unsubscribe(Observer observer);
+    public void notifyObservers();
+    public String getUpdate();
+
+
+
+}
